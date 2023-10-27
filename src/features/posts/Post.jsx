867cc73f593e0bc8ede6c834f72a95b68e6c565d
Jesse Vasquez <jr90box@gmail.com>
2023-10-27 13:48:22 -0700

    added router provider and removed app

--- src/features/posts/Post.jsx
+++ src/features/posts/Post.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Post({ post }) {
   return (
     <li>
       <h2>{post.title}</h2>
       <p>{post.price}</p>
       <p>{post.description}</p>
+      <Link to={`/posts/${post._id}`}>See Details</Link>
     </li>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
 import "./index.css";
 import store from "./store/store.js";
 import { Provider } from "react-redux";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PostDetails from "./features/posts/PostDetails.jsx";
+import PostPage from "./features/posts/PostPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PostPage />,
+  },
+  {
+    path: "/posts",
+    element: <PostPage />,
+  },
+  {
+    path: "/posts/:id",
+    element: <PostDetails />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
