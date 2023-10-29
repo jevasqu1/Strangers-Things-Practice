6e693068e1b5b656c88cdc64d42c6d287c4ace2f
Jesse Vasquez <jr90box@gmail.com>
2023-10-28 18:26:50 -0700

    added Navbar and updated root

---
+++ src/features/layout/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>{"Stranger's Things"}</h1>
+      <menu>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+      </menu>
+    </nav>
+  );
+}
